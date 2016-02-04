1e1f52712399fd61a8212f5244c4637fc7ce66bb
Daffelatte <Olof.Olivecrona@gmail.com>
2016-02-05 00:54:19 +0100

    More compact code

--- src/crocodile_hunter/Player.java
+++ src/crocodile_hunter/Player.java
@@ -1,9 +1,13 @@
 package crocodile_hunter;
 import crocodile_hunter.main;
 
+
 public class Player {
 	static int positionX = 0;
 	static int positionY = 9;
+	static int deltaX = 0;
+	static int deltaY = 0;
+	
 	static boolean[][] playerMap = {
 			{false,false,false,false,false,false,false,false,false,false},
 			{false,false,false,false,false,false,false,false,false,false},
@@ -18,6 +22,58 @@ public class Player {
 	};
 	public Player(){
 	}
+	static String executeCommand(int intCommand){
+		String strReturn = null;
+		for (int i=0;main.intCommandList.length>i;i++){
+			if (main.intCommandList[i]==intCommand){
+				if (i<4){
+					deltaY=main.positionDelta[0][i];
+					deltaX=main.positionDelta[1][i];
+					strReturn=Player.walk(deltaY,deltaX,i);
+				}else if (i==4){
+					strReturn=Player.generateMap();
+				}else if (i==5){
+					strReturn=main.strCommands;
+				}else if(i==6){
+					strReturn="You embrace death.";
+					main.isRunning=40;
+				};
+				//strReturn=main.strCommandList[i];
+				break;
+			}
+		}
+		
+		return strReturn;
+	}
+	static String walk(int deltaY, int deltaX, int i){
+		String strReturn = null;
+		if (positionY+deltaY!=10 && positionY+deltaY!=-1 && positionX+deltaX!=10 && positionX+deltaX!=-1){
+			playerMap[positionY][positionX]=false;
+			positionY+=deltaY;
+			positionX+=deltaX;
+			playerMap[positionY][positionX]=true;
+			strReturn=main.strMoveList[0][i];
+		}else{
+			strReturn=main.strMoveList[1][i];
+		};
+		return (strReturn);
+	};
+	static String generateMap(){
+		String strReturn = "You look at your map.\n\n_|0|1|2|3|4|5|6|7|8|9|\n";
+		for(int i=0;i<10;i++) {
+			strReturn+=i+"|";
+            for(int j=0;j<10;j++) {
+            	if (Player.playerMap[i][j])
+            		strReturn+="X|";
+            	else{
+            		strReturn+="_|";
+            	}
+            }
+            strReturn+="\n";
+        }
+		return (strReturn);
+	};
+	/*
 	static String walk(int direction){
 		String strDirection="[error: direction 0]";
 		switch(direction){
@@ -70,6 +126,6 @@ public class Player {
 				break;
 		}
 		return (strDirection);
-	}
+	}*/
 
 }
