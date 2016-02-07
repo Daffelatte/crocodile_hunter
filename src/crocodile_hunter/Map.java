e56e72560d94d70f1f81eda34e915a4af169f1e7
Daffelatte <Olof.Olivecrona@gmail.com>
2016-02-07 22:05:21 +0100

    nothing
    I only swapped from full to semi

--- src/crocodile_hunter/Map.java
+++ src/crocodile_hunter/Map.java
@@ -31,7 +31,7 @@ public class Map{
 	//static String[] strSemiASCIIList = {" ","~","~","~","~","-","-","-","-","-"};
 	static String[] strSemiASCIIList = {" ","-","-","-","-"," "," "," "," "," "  ," "," "," "," "," "};
 	//static String[] strASCIIList = {"0","1","2","3","4","5","6","7","8","9","A","B"};
-	static String[] strActiveASCIIList=strFullASCIIList;
+	static String[] strActiveASCIIList=strSemiASCIIList;
 	static int intNumOfPeaks=1;
 	public static int[][] intActiveMap;
 	public Map(){
